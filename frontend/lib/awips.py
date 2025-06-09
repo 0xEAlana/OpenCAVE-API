607b2e088ccda9d8e8dbb6c3380d235e99ce9048
Alana E <cemery12@asu.edu>
2025-06-09 16:53:22 -0700

    updt

--- frontend/lib/awips.py
+++ frontend/lib/awips.py
@@ -1,3 +1,25 @@
+import metapy
+import json
+import requests
+import os
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.patches as ptches 
+#Jesus christ the imports
+import base64
+import datetime
+import time
+import io 
+import metpy.calc as mpcalc
+import metpy.plots as mpplots
+from metpy.units import units
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from metpy.plots import add_metpy_logo
+from_init__.py file in the lib directory to metpy.plots import SkewT
+from awips.dataaccess import DataAccessLayer
+
+
 class AWIPScli:
     def __init__(self, awipsserver= 'edex-cloud.unidata.ucar.edu'):
         
@@ -7,3 +29,7 @@ class AWIPScli:
             'unidata.ucar.edu',
         ]
         self.altserver = awipsservers
+# Now we can finally get to writing the actual code, once you have the imports out of the way may take roughly 10 mins to install
+self.awipsserver = awipsserver 
+self._connect(self):
+
